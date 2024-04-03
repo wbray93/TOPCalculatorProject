43319786c2e9cd61c2c989ea467809604819c5fc
wbray93 <wbray053@gmail.com>
2024-04-03 15:17:41 -0700

    Initial commit plus use case 1 and 2 finished.

---
+++ script.js
@@ -0,0 +1,24 @@
+let calculationFirstNumber;
+let calculationSecondNumber;
+let operator;
+
+
+function add(num1, num2) {
+    let result = num1 + num2;
+    console.log(result)
+}
+
+function subtract(num1, num2) {
+    let result = num1 - num2;
+    console.log(result)
+}
+
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    console.log(result)
+}
+
+function divide(num1, num2) {
+    let result = num1 / num2;
+    console.log(result)
+}
