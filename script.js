1ed0cd0b287a55c5d663c6bc8665190de3f8df36
wbray93 <wbray053@gmail.com>
2024-04-03 19:31:03 -0700

    Completed use case 3 and 4 next thing to do is use case 5 which is to create the display update function.

--- script.js
+++ script.js
@@ -22,3 +22,19 @@ function divide(num1, num2) {
     let result = num1 / num2;
     console.log(result)
 }
+
+function operate(num1, operator, num2) {
+    if(operator === "+") {
+        return add(num1, num2);
+    } else if(operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2)
+    } else if (operator === "/") {
+        return divide(num1, num2);
+    }
+    console.log(operate)
+}
+
+let buttonValueTest = document.querySelector("value")
+console.log(buttonValueTest)
