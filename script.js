994e37a9e8244774d8696751f53fdbf11cc122fa
wbray93 <wbray053@gmail.com>
2024-04-06 20:49:41 -0700

    Got the operators to properly append to the screen and got the equals button to do something however its still only pushing undefined to the screen. Need to solve the issue of storing the value before the operator and after the operator into num1 and num2

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let calculationFirstNumber;
 let calculationSecondNumber;
 let operator;
-
+let displayValue;
+let result;
+const calculatorDisplay = document.querySelector('[data-display]');
+const calculatorOperatorButtons = document.querySelectorAll('[data-operator]');
+const clearButton = document.querySelector('[data-all-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+const equalsButton = document.querySelector('[data-equals]');
+const calculatorNumberButtons = document.querySelectorAll('[data-number]');
 
 function add(num1, num2) {
     let result = num1 + num2;
@@ -23,48 +30,55 @@ function divide(num1, num2) {
     console.log(result)
 }
 
-function operate(num1, operator, num2) {
-    if(operator === "+") {
+
+//unsure how to pass my data-operator attibute here. im assuming i pass the equals button
+function operate(num1, calculatorOperatorButtons, num2) {
+    if (calculatorOperatorButtons === "+") {
         return add(num1, num2);
-    } else if(operator === "-") {
+    } else if (calculatorOperatorButtons === "-") {
         return subtract(num1, num2);
-    } else if (operator === "*") {
+    } else if (calculatorOperatorButtons === "*") {
         return multiply(num1, num2)
-    } else if (operator === "/") {
+    } else if (calculatorOperatorButtons === "/") {
         return divide(num1, num2);
     }
-    console.log(operate)
+    updateCalculatorDisplay();
 }
 
-
-
-const calculatorDisplay = document.querySelector('[data-display]');
-const calculatorOperatorButtons =  document.querySelectorAll('[data-operator]');
-const clearButton = document.querySelector('[data-all-clear]');
-const deleteButton = document.querySelector('[data-delete]');
-const equalsButton = document.querySelector('[data-equals]');
-const calculatorNumberButtons = document.querySelectorAll('[data-number]');
-
 function updateCalculatorDisplay() {
-
+    calculatorDisplay.placeholder = result;
 }
 
 function clearDisplay() {
-    calculatorDisplay.placeholder="0"
+    calculatorDisplay.placeholder = ""
 }
 
-clearButton.addEventListener('click', () =>  {
+clearButton.addEventListener('click', () => {
     clearDisplay();
 })
 
 deleteButton.addEventListener('click', () => {
-    
+
 })
+
+equalsButton.addEventListener('click', () => {
+    operate();
+});
 
 calculatorNumberButtons.forEach(button => button.addEventListener('click', () => {
     let buttonContent = button.innerText;
-    calculatorDisplay.placeholder = buttonContent;
-    console.log(buttonContent)
+    displayValue = buttonContent;
+    calculatorDisplay.placeholder += displayValue;
+    console.log(displayValue);
 }))
 
+calculatorOperatorButtons.forEach(button => button.addEventListener('click', () => {
+    let operatorContent = button.innerText;
+    displayValue = operatorContent;
+    calculatorDisplay.placeholder += operatorContent;
+}));
+
+
 console.log(calculatorNumberButtons);
+
+console.log(result)
