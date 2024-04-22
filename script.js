462d1b65463a66df61ab61dbfda0a66c1d5a6eda
wbray93 <wbray053@gmail.com>
2024-04-22 15:27:23 -0700

    This gets the functionality of the calculator to start working still need to fix issues with it properly storing the calculations as I am running into errors at times withe calculations.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let calculationFirstNumber;
 let calculationSecondNumber;
 let operator;
-let displayValue;
+let displayValue = "";
 let result;
 const calculatorDisplay = document.querySelector('[data-display]');
 const calculatorOperatorButtons = document.querySelectorAll('[data-operator]');
@@ -11,71 +11,77 @@ const equalsButton = document.querySelector('[data-equals]');
 const calculatorNumberButtons = document.querySelectorAll('[data-number]');
 
 function add(num1, num2) {
-    let result = num1 + num2;
+    result = num1 + num2;
     console.log(result)
 }
 
 function subtract(num1, num2) {
-    let result = num1 - num2;
+    result = num1 - num2;
     console.log(result)
 }
 
 function multiply(num1, num2) {
-    let result = num1 * num2;
+    result = num1 * num2;
     console.log(result)
 }
 
 function divide(num1, num2) {
-    let result = num1 / num2;
+    result = num1 / num2;
     console.log(result)
 }
 
 
 //unsure how to pass my data-operator attibute here. im assuming i pass the equals button
-function operate(num1, calculatorOperatorButtons, num2) {
-    if (calculatorOperatorButtons === "+") {
-        return add(num1, num2);
-    } else if (calculatorOperatorButtons === "-") {
-        return subtract(num1, num2);
-    } else if (calculatorOperatorButtons === "*") {
-        return multiply(num1, num2)
-    } else if (calculatorOperatorButtons === "/") {
-        return divide(num1, num2);
+function operate() {
+    if (operator === "+") {
+        add(calculationFirstNumber, calculationSecondNumber);
+    } else if (operator === "-") {
+        subtract(calculationFirstNumber, calculationSecondNumber);
+    } else if (operator === "*") {
+        multiply(calculationFirstNumber, calculationSecondNumber)
+    } else if (operator === "/") {
+        divide(calculationFirstNumber, calculationSecondNumber);
     }
-    updateCalculatorDisplay();
+    updateCalculatorDisplay(result);
 }
 
-function updateCalculatorDisplay() {
-    calculatorDisplay.placeholder = result;
-}
+function updateCalculatorDisplay(value) {
+    calculatorDisplay.placeholder = value;
+};
 
 function clearDisplay() {
-    calculatorDisplay.placeholder = ""
-}
+    displayValue = "";
+    updateCalculatorDisplay(displayValue);
+};
 
-clearButton.addEventListener('click', () => {
-    clearDisplay();
-})
+clearButton.addEventListener('click', clearDisplay);
 
 deleteButton.addEventListener('click', () => {
-
+    displayValue = displayValue.slice(0, -1);
+    updateCalculatorDisplay(displayValue);
 })
 
 equalsButton.addEventListener('click', () => {
+    const [num1Str, operator, num2Str] = displayValue.split(/(\+|\-|\*|\/)/);
+    calculationFirstNumber = parseInt(num1Str)
+    calculationSecondNumber = parseInt(num2Str);
     operate();
 });
 
 calculatorNumberButtons.forEach(button => button.addEventListener('click', () => {
     let buttonContent = button.innerText;
-    displayValue = buttonContent;
-    calculatorDisplay.placeholder += displayValue;
-    console.log(displayValue);
+    if(operator !== undefined && result === undefined) {
+        clearDisplay();
+    }
+    displayValue += buttonContent;
+    updateCalculatorDisplay(displayValue);
 }))
 
 calculatorOperatorButtons.forEach(button => button.addEventListener('click', () => {
-    let operatorContent = button.innerText;
-    displayValue = operatorContent;
-    calculatorDisplay.placeholder += operatorContent;
+    operator = button.innerText;
+    calculationFirstNumber = parseInt(displayValue);
+    displayValue += operator;
+    updateCalculatorDisplay(displayValue);
 }));
 
 
